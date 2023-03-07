72f21ed49fe938d57f47f6418a4c00eb50661fa4
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-07 15:31:27 +0530

    Miscellaneous setup

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Fragment } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Chat from "./components/Chat/Chat";
 
 const App = () => {
   return (
