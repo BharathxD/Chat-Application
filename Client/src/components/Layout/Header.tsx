912bd0daf19621d5d4991c9a6ed3cf56b9e34858
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-08 20:40:09 +0530

    Destructured chat.title and chat.description inside the component

--- Client/src/components/Layout/Header.tsx
+++ Client/src/components/Layout/Header.tsx
@@ -2,24 +2,24 @@ import React from "react";
 import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
 import { ChatHeaderProps } from "react-chat-engine-advanced";
 
-type HeaderProps = {
+interface HeaderProps {
   chat: ChatHeaderProps;
-};
+}
 
 const Header: React.FC<HeaderProps> = ({ chat }) => {
+  const { title, description } = chat;
+
   return (
     <div className="chat-header">
       <div className="flexbetween">
         <ChatBubbleLeftRightIcon className="icon-chat" />
-        <h3 className="header-text">{chat.title}</h3>
+        <h3 className="header-text">{title}</h3>
       </div>
       <div className="flexbetween">
         <PhoneIcon className="icon-phone" />
-        {chat.description !== "⬅️ ⬅️ ⬅️" ? (
-          <p className="header-text">{chat.description}</p>
-        ) : (
-          <p className="header-text">no chat selected</p>
-        )}
+        <p className="header-text">
+          {description !== "⬅️ ⬅️ ⬅️" ? description : "no chat selected"}
+        </p>
       </div>
     </div>
   );
